10ee7cfffaf510f55e160f4b4b252a3c90bcad51
Sam Vedernikoff <samvedernikoff@Sams-MacBook-Pro-6.local>
2019-04-30 17:44:15 -0700

    rough draft

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './App.css';
+import Home   from './components/pages/home';
+import SearchTickets   from './components/pages/searchTickets';
+// import PostTickets   from './components/pages/PostTickets';
+import ViewMyTickets   from './components/pages/viewMyTickets';
+// import CheckInbox   from './components/pages/CheckInbox';
+// import ViewProfile   from './components/pages/ViewProfile';
+
+function App() {
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path='/' component={Home}/>
+				<Route exact path='/Home' component={Home}/>
+				<Route exact path='/SearchTickets' component={SearchTickets} />
+				<Route exact path='/PostTickets' component={Home}/>
+				<Route exact path='/ViewMyTickets' component={ViewMyTickets} />
+				<Route exact path='/CheckInbox' component={Home}/>
+				<Route exact path='/ViewProfile' component={Home} />
+			</div>
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/components/pages/home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+	render() {
+		return (
+            <header className="App-header">
+                <h1>Welcome to Ride+</h1>
+                <h3>
+                    <nav>
+                        <ul>
+                            <Link to="/SearchTickets">Search Tickets</Link>
+                            <br />
+                            <Link to="/Home">Post Tickets</Link>
+                            <br />
+                            <Link to="/ViewMyTickets">View My Tickets</Link>
+                            <br />
+                            <Link to="/Home">Check Inbox</Link>
+                            <br />
+                            <Link to="/Home">View Profile</Link>
+                        </ul>
+                    </nav>
+                </h3>
+            </header>
+		);
+	}
+}
+export default Home;
